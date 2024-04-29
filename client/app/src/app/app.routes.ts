17856cad85b19bbd0df3d3688e0520d3e2c731c2
Kaloyan Kateliev <thewolfyplay@gmail.com>
2024-04-29 15:12:03 +0300

    routes

--- client/app/src/app/app.routes.ts
+++ client/app/src/app/app.routes.ts
@@ -1,3 +1,71 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     pathMatch: 'full',
+    //     redirectTo: 'dashboard',
+    // },
+    {
+    path: 'dashboard',
+    // canActivate: [authGuard],
+    component: DashboardComponent
+    },
+    // {
+    //     path: 'profile',
+    //     canActivate: [authGuard]
+    //     component: ProfileComponent
+    // }
+
+    // {
+    //     path: 'login',
+    //     component: LoginComponent,
+    //     canActivate: [authGuard],
+    //     data: {
+    //         notAuthenticatedRequired: true,
+    //     },
+    // },
+
+    // {
+    // path: 'register',
+    // component: RegisterComponent,
+    // canActivate: [authGuard],
+    // data: {
+    //     notAuthenticatedRequired: true,
+    //  },
+    // },
+
+    // {
+    //     path: 'todos',
+    //     // canActivate: [authGuard],
+    //     children: [
+    //         {
+    //           path: '',
+    //           pathMatch: 'full',
+    //           component: TodosComponent,
+    //         },
+    //         {
+    //           path: ':id',
+    //           component: TodosUserComponent,
+    //         },
+    //       ],
+    // },
+
+    // {
+    //     path: 'assistant',
+    //     // canActivate: [authGuard],
+    //     component: HomeAssistantComponent
+    // },
+
+    // {
+    //     path: 'Schedular',
+    //     // canActivate: [authGuard],
+    //     component: SchedularComponent
+    // },
+
+    // {
+    // path: '**',
+    // component: NotFoundComponent,
+    // },
+];
